523802bfc3a512b6a71ca4b37ccceb59045800f1
miguelsabino23 <miguel2012palhada@gmail.com>
2025-01-17 12:10:04 -0300

    .

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const serverless = require("serverless-http");
 
 server.use(middlewares);
 server.use(router);
-
-module.exports = serverless(server);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
